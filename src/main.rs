456ac8e8ad6b10c71734581ae695bb7b527663e3
tzip <tris.zip@gmai.com>
2020-03-11 00:45:01 -0400

    command test for reformat

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ use std::env;
 use std::thread;
 use serenity::client::validate_token;
 use serenity::utils::MessageBuilder;
-use serenity::model::gateway::Activity;
+use serenity::model::gateway::{Activity, Ready};
 use std::sync::Arc;
 use std::time::{Duration, SystemTime};
 use serenity::model::guild::{Guild, Member};
@@ -38,6 +38,8 @@ use commands::{
     bot_commands::*,
 };
 use crate::commands::spreadsheet;
+use serenity::model::event::ResumedEvent;
+
 #[group]
 #[commands(servers,ping,about)]
 struct General;
@@ -45,51 +47,56 @@ struct General;
 
 struct Handler;
 impl EventHandler for Handler {
-    fn message(&self, ctx: Context, msg: Message) {
-
-        let activity = "use 'help;' for spreadsheet commands";
-        ctx.set_activity(Activity::playing(&activity));
-        if (msg.content.starts_with(";")|| msg.content.ends_with(";")) && msg.content.len() > 1 {
-            let input = &msg.content.replace(";","");
-            let mut input_arr:Vec<String> = input.splitn(2," ").map(|x| x.to_string()).collect();
-            match input_arr[0].to_uppercase().as_ref(){
-                "HELP"=>{
-                    let url = "https://discordapp.com/api/oauth2/authorize?client_id=684150439721304095&permissions=0&scope=bot";
-                    let help = format!(">>> Spreadsheet-bot command basics:\n\
-                     -Every command for spreadsheet-bot  starts with the prefix `;` followed by a cell to reference on the sheet\n\
-                     -A reference to a cell is done by the column letter followed by row number (ex: `a1`)\n\
-                     -A cell can be set by a cell reference followed by a equal sign ( separated by a space ) (ex: `a1  = 2`)\n\
-                     -A cell can be set to a string, instead of a number, when quotes are in place ( ex: `a1 = \"hello world\" `)\n\
-                     -A cell could also reference other cells by putting a cell reference in the deceleration (ex: `a1 = ( b1 * 2 )` )\n\
-                     they can also reference multiple cells\n\n\
-                     -Spreadsheet can be printed with `;spread`, `;spreadsheet` ,or `;print`\n\
-                     -Spreadsheet can be cleared with the `;clear` command, or combined with a cell ref to clear a cell (ex: `;clear a1`)\n\n\
-                     The spreadsheet is the same for every server that it is on and can be changed by anyone\n\
-                     Creator: ***Chilla#4568***\n\
-                      invite the bot with this link: {}",url);
-                    if let Err(why) = msg.author.direct_message(ctx,|ret|{
-                        ret.embed(|r|
-                            r.description(&help).color((0,255,0))
-
-                        );
-                        ret
-                    }){
-                        println!("Error sending message: {:?}",why);
-                    };
-                }
-                _=>{
-                    let mut  l = spreadsheet::enter_command(input.parse().unwrap());
-
-                    println!("username:{},command:{}",msg.author.name,msg.content);
-                    println!("user id:{}, username:{}, spreadsheet \n{}",msg.author.id,msg.author.name,l);
-                    l = format!("\n```{}```",l);
-                    if let Err(why) =  msg.reply(ctx,l ){
-                        println!("Error sending message: {:?}",why);
-                    };
-                }
-            }
-        }
+    fn ready(&self,_:Context,ready: Ready){
+        info!("Connected as {}", ready.user.name);
     }
+    fn resume(&self,_:Context,_:ResumedEvent){
+        info!("Resumed");
+    }
+    // fn message(&self, ctx: Context, msg: Message) {
+    //     let activity = "use 'help;' for spreadsheet commands";
+    //     ctx.set_activity(Activity::playing(&activity));
+    //     if (msg.content.starts_with(";")|| msg.content.ends_with(";")) && msg.content.len() > 1 {
+    //         let input = &msg.content.replace(";","");
+    //         let mut input_arr:Vec<String> = input.splitn(2," ").map(|x| x.to_string()).collect();
+    //         match input_arr[0].to_uppercase().as_ref(){
+    //             "HELP"=>{
+    //                 let url = "https://discordapp.com/api/oauth2/authorize?client_id=684150439721304095&permissions=0&scope=bot";
+    //                 let help = format!(">>> Spreadsheet-bot command basics:\n\
+    //                  -Every command for spreadsheet-bot  starts with the prefix `;` followed by a cell to reference on the sheet\n\
+    //                  -A reference to a cell is done by the column letter followed by row number (ex: `a1`)\n\
+    //                  -A cell can be set by a cell reference followed by a equal sign ( separated by a space ) (ex: `a1  = 2`)\n\
+    //                  -A cell can be set to a string, instead of a number, when quotes are in place ( ex: `a1 = \"hello world\" `)\n\
+    //                  -A cell could also reference other cells by putting a cell reference in the deceleration (ex: `a1 = ( b1 * 2 )` )\n\
+    //                  they can also reference multiple cells\n\n\
+    //                  -Spreadsheet can be printed with `;spread`, `;spreadsheet` ,or `;print`\n\
+    //                  -Spreadsheet can be cleared with the `;clear` command, or combined with a cell ref to clear a cell (ex: `;clear a1`)\n\n\
+    //                  The spreadsheet is the same for every server that it is on and can be changed by anyone\n\
+    //                  Creator: ***Chilla#4568***\n\
+    //                   invite the bot with this link: {}",url);
+    //                 if let Err(why) = msg.author.direct_message(ctx,|ret|{
+    //                     ret.embed(|r|
+    //                         r.description(&help).color((0,255,0))
+    //
+    //                     );
+    //                     ret
+    //                 }){
+    //                     println!("Error sending message: {:?}",why);
+    //                 };
+    //             }
+    //             _=>{
+    //                 let mut  l = spreadsheet::enter_command(input.parse().unwrap());
+    //
+    //                 println!("username:{},command:{}",msg.author.name,msg.content);
+    //                 println!("user id:{}, username:{}, spreadsheet \n{}",msg.author.id,msg.author.name,l);
+    //                 l = format!("\n```{}```",l);
+    //                 if let Err(why) =  msg.reply(ctx,l ){
+    //                     println!("Error sending message: {:?}",why);
+    //                 };
+    //             }
+    //         }
+    //     }
+    // }
 }
 
 
